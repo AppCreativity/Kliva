c3999338618865b9069dcd40baba30b7e6baff61
Depechie <glenn.versweyveld@gmail.com>
2015-08-20 00:22:50 +0200

    Page layout done with device family triggers

--- Kliva/Views/LoginPage.xaml.cs
+++ Kliva/Views/LoginPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿
-using System;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
+﻿using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -17,21 +12,6 @@ namespace Kliva.Views
         public LoginPage()
         {
             this.InitializeComponent();
-
-            //TODO: Glenn - We need this to change into Device Family state trigger, but doesn't seem to work?
-            if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
-            {
-                //this.LayoutRoot.Background = new ImageBrush() { ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Merckx_1920.jpg", UriKind.Absolute)), Stretch = Stretch.UniformToFill };
-                //this.ContentPanel.Margin = new Thickness() { Right = 0, Top = 0, Left = 0, Bottom = 100 };
-                //this.ContentPanel.HorizontalAlignment = HorizontalAlignment.Center;
-            }
-
-            if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
-            {
-                //this.LayoutRoot.Background = new ImageBrush() { ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Brooks.jpg", UriKind.Absolute)), Stretch = Stretch.UniformToFill };
-                //this.ContentPanel.Margin = new Thickness() { Right = 10, Top = 0, Left = 10, Bottom = 10 };
-                //this.ContentPanel.HorizontalAlignment = HorizontalAlignment.Left;
-            }
         }
     }
 }
