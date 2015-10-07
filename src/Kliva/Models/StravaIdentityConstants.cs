a05aa3066c493c682bbbc95ba0fc83a2e1e76b18
bartlannoeye <bart.lannoeye@gmail.com>
2015-10-08 00:47:02 +0200

    Fixing build

---
+++ src/Kliva/Models/StravaIdentityConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Kliva.Models
+{
+    public static class StravaIdentityConstants
+    {
+        public const string STRAVA_AUTHORITY_CLIENT_ID = "0";
+        public const string STRAVA_AUTHORITY_CLIENT_SECRET = "riiiiight :)";
+    }
+}
