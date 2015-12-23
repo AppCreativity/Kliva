718ef0fd295d3c5e771938b831d9cb5e95c7e43f
Depechie <glenn.versweyveld@gmail.com>
2015-12-23 14:32:55 +0100

    Cleanup code

--- src/Kliva/Views/LoginPage.xaml.cs
+++ src/Kliva/Views/LoginPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Kliva.Views
 {
     /// <summary>
--- src/Kliva/Views/MainPage.xaml.cs
+++ src/Kliva/Views/MainPage.xaml.cs
@@ -4,8 +4,6 @@ using Kliva.ViewModels.Interfaces;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Kliva.Views
 {
     /// <summary>
