0f3a596646b71abe0e0a486ec4682d7c3b715db0
Depechie <glenn.versweyveld@gmail.com>
2016-01-03 19:56:37 +0100

    Small listview selection tweak

--- src/Kliva/Controls/SidePaneControl.xaml.cs
+++ src/Kliva/Controls/SidePaneControl.xaml.cs
@@ -5,6 +5,8 @@ namespace Kliva.Controls
 {
     public sealed partial class SidePaneControl : UserControl
     {
+        private bool _listViewChanging = false;
+
         private SidePaneViewModel ViewModel => DataContext as SidePaneViewModel;
 
         public SidePaneControl()
@@ -13,6 +15,26 @@ namespace Kliva.Controls
 
             //Needed when using x:Bind in the SidePane control XAML
             DataContextChanged += (sender, args) => this.Bindings.Update();
+        }
+
+        private void OnBottomMenuSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_listViewChanging)
+            {
+                _listViewChanging = true;
+                TopMenu.SelectedIndex = -1;
+                _listViewChanging = false;
+            }
+        }
+
+        private void OnTopMenuSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_listViewChanging)
+            {
+                _listViewChanging = true;
+                BottomMenu.SelectedIndex = -1;
+                _listViewChanging = false;
+            }            
         }
     }
 }
