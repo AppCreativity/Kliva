f70387e5dd5154c330273e29dec500095690433b
Depechie <glenn.versweyveld@gmail.com>
2016-02-24 20:24:38 +0100

    Move to top button in activity list

--- src/Kliva/Controls/ActivityFeedControl.xaml.cs
+++ src/Kliva/Controls/ActivityFeedControl.xaml.cs
@@ -1,16 +1,72 @@
-﻿using Kliva.ViewModels.Interfaces;
+﻿using System.Diagnostics;
+using System.Linq;
+using Windows.UI.Xaml;
+using Kliva.ViewModels.Interfaces;
 using Windows.UI.Xaml.Controls;
+using Cimbalino.Toolkit.Extensions;
+using Kliva.Helpers;
 
 namespace Kliva.Controls
 {
     public sealed partial class ActivityFeedControl : UserControl
     {
+        private ScrollViewer _scrollViewer;
+        private bool _isAtTop = true;
+
         private IStravaViewModel ViewModel => DataContext as IStravaViewModel;
 
         public ActivityFeedControl()
         {
             this.InitializeComponent();
             DataContextChanged += (sender, args) => this.Bindings.Update();
-        }        
+        }
+
+        private void OnActivityListLoaded(object sender, RoutedEventArgs e)
+        {
+            _scrollViewer = ActivityList.GetVisualDescendents<ScrollViewer>().FirstOrDefault();
+            if (_scrollViewer != null)
+            {
+                _scrollViewer.ViewChanged += OnScrollViewerViewChanged;
+                _scrollViewer.ViewChanging += OnScrollViewerViewChanging;
+                _scrollViewer.IsScrollInertiaEnabled = true;
+            }
+        }
+
+        private void OnScrollViewerViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
+        {
+            if (_scrollViewer == null)
+                return;
+
+            if (e.IsInertial)
+            {
+                if (!_isAtTop)
+                {
+                    VisualStateManager.GoToState(this, "ShowGoToButton", true);
+                }
+            }
+        }
+
+        private void OnScrollViewerViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            if (_scrollViewer == null)
+                return;
+
+            _isAtTop = _scrollViewer.VerticalOffset < 100;
+
+            if (_isAtTop)
+            {
+                VisualStateManager.GoToState(this, "HideGoToButton", true);
+            }
+        }
+
+        private void OnGoToTopButtonClick(object sender, RoutedEventArgs e)
+        {
+            ScrollToTop();
+        }
+
+        private void ScrollToTop()
+        {
+            _scrollViewer?.ScrollToVerticalOffsetWithAnimation(0);
+        }
     }
 }
