d9a9ba8b56df4440e9fcdc3f867cdaead7060fd2
Depechie <glenn.versweyveld@gmail.com>
2016-03-13 13:49:08 +0100

    Able to switch between KM or Miles

--- src/Kliva/ViewModels/SettingsViewModel.cs
+++ src/Kliva/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,11 @@
-﻿using Cimbalino.Toolkit.Services;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Cimbalino.Toolkit.Services;
+using GalaSoft.MvvmLight.Command;
+using Kliva.Helpers;
+using Kliva.Models;
 using Kliva.Services.Interfaces;
 
 namespace Kliva.ViewModels
@@ -9,9 +16,44 @@ namespace Kliva.ViewModels
 
         public string AppVersion => _settingsService.AppVersion.ToString();
 
+        private ObservableCollection<string> _measurementUnits = new ObservableCollection<string>();
+        public ObservableCollection<string> MeasurementUnits
+        {
+            get { return _measurementUnits; }
+            set { Set(() => MeasurementUnits, ref _measurementUnits, value); }
+        }
+
+        private string _selectedMeasurementUnit;
+        public string SelectedMeasurementUnit
+        {
+            get { return _selectedMeasurementUnit; }
+            set { Set(() => SelectedMeasurementUnit, ref _selectedMeasurementUnit, value); }
+        }
+
+        private RelayCommand _viewLoadedCommand;
+        public RelayCommand ViewLoadedCommand => _viewLoadedCommand ?? (_viewLoadedCommand = new RelayCommand(async () => await ViewLoaded()));
+
         public SettingsViewModel(INavigationService navigationService, ISettingsService settingsService) : base(navigationService)
         {
             _settingsService = settingsService;
+
+            this.PropertyChanged += OnSettingsViewModelPropertyChanged;
+
+            //TODO: Glenn - Translate enums!
+            MeasurementUnits.Add(DistanceUnitType.Kilometres.ToString());
+            MeasurementUnits.Add(DistanceUnitType.Miles.ToString());
+        }
+
+        private async void OnSettingsViewModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("SelectedMeasurementUnit", StringComparison.OrdinalIgnoreCase))
+                await _settingsService.SetDistanceUnitTypeAsync(Enum<DistanceUnitType>.Parse(SelectedMeasurementUnit));
+        }
+
+        private async Task ViewLoaded()
+        {
+            DistanceUnitType distanceUnitType = await _settingsService.GetStoredDistanceUnitTypeAsync();
+            SelectedMeasurementUnit = distanceUnitType.ToString();
         }
     }
 }
