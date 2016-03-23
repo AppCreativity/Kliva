2b5fbe868c371fe7996404fdbbe01c7f19dce41c
Tim Heuer <tim@timheuer.com>
2016-03-23 13:41:14 -0700

    Adding implicit to profile friends view

--- src/Kliva/Views/ProfilePage.xaml.cs
+++ src/Kliva/Views/ProfilePage.xaml.cs
@@ -2,6 +2,9 @@
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using Kliva.ViewModels;
+using Windows.UI.Composition;
+using ImplicitAnimations;
+using System;
 
 namespace Kliva.Views
 {
@@ -15,6 +18,27 @@ namespace Kliva.Views
         public ProfilePage()
         {
             this.InitializeComponent();
+        }
+
+        private void OnFriendsContainerChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            // get the Visual for the item container
+            var visual = args.ItemContainer.GetVisual(); // this is the GridViewItem
+
+            // get the instance of the compositor
+            Compositor compositor = visual.Compositor;
+
+            // create the animation we want to run implicitly when the property changes
+            var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
+            offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+            offsetAnimation.Duration = TimeSpan.FromMilliseconds(450);
+
+            // create the implicit map to the properties we care about
+            var implicitMap = compositor.CreateImplicitAnimationMap();
+            implicitMap.Add("Offset", offsetAnimation);
+
+            // set the implicit map to the visual
+            visual.ImplicitAnimations = implicitMap;
         }
     }
 }
