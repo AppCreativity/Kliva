c567683c75b1f24f50e8923ab4e225e7c593ac69
Depechie <glenn.versweyveld@gmail.com>
2016-04-12 22:41:22 +0200

    Pivot icons for Mobile screen size

---
+++ src/Kliva/Controls/IconPivotHeader.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Kliva.Controls
+{
+    public sealed partial class IconPivotHeader : UserControl
+    {
+        public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register("Glyph", typeof(string), typeof(IconPivotHeader), null);
+
+        public string Glyph
+        {
+            get { return GetValue(GlyphProperty) as string; }
+            set { SetValue(GlyphProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(IconPivotHeader), null);
+
+        public string Label
+        {
+            get { return GetValue(LabelProperty) as string; }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public IconPivotHeader()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
