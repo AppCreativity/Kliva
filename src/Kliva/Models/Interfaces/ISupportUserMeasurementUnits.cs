6a4998b1c5fcbc9afc5bcf5434c99066d412ac14
Pieter Nijs <pieternijs@live.be>
2016-09-25 11:30:41 +0200

    Finalize UnitConversions
    Added UserMeasurementUnitMetric properties to 'Stats', 'SegmentEffort',
    'SegmentSummary', 'LeaderboardEntry'
    Added ISupportUserMeasurementUnits interface to make code in
    StravaService class more elegant

---
+++ src/Kliva/Models/Interfaces/ISupportUserMeasurementUnits.cs
@@ -0,0 +1,8 @@
+﻿namespace Kliva.Models.Interfaces
+{
+    public interface ISupportUserMeasurementUnits
+    {
+        DistanceUnitType MeasurementUnit { get; }
+        void SetUserMeasurementUnits(DistanceUnitType measurementUnit);
+    }
+}
