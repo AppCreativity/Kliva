ebdc98a90974a0d09fed49eb673ce7fa66b98a3b
Bart Lannoeye <bart.lannoeye@gmail.com>
2016-11-03 20:38:19 +0100

    Debug keys
    Fixes #92

--- src/Kliva/Models/StravaIdentityConstants.cs
+++ src/Kliva/Models/StravaIdentityConstants.cs
@@ -2,8 +2,9 @@
 {
     public static class StravaIdentityConstants
     {
-        public const string STRAVA_AUTHORITY_CLIENT_ID = "0";
-        public const string STRAVA_AUTHORITY_CLIENT_SECRET = "riiiiight :)";
+        // These keys should be used for testing/debugging purposes only.
+        public const string STRAVA_AUTHORITY_CLIENT_ID = "14473";
+        public const string STRAVA_AUTHORITY_CLIENT_SECRET = "2d438723e18ffe0a4a2711d1e873013407129515";
         public const string GOOGLE_MAP_API = "bliep bliep";
     }
 }
